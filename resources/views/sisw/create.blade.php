61a8e9fddac724554226282c1b63c95129f04476
ArdianJimmy <116485220+ArdianJimmy@users.noreply.github.com>
2022-12-30 10:35:34 +0700

    uas framework

---
+++ resources/views/sisw/create.blade.php
@@ -0,0 +1,100 @@
+<!-- Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+<!-- Styles -->
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+<div class="font-sans antialiased">
+    @include('layouts.navigation')
+
+   
+</div>
+</div>
+@extends('template')
+
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Create New Mahasiswa</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('sisw.index') }}"> Kembali</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> Input gagal.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('sisw.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+     <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIM:</strong>
+                <input type="text" name="nim" class="form-control" placeholder="nim SISWA">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="nama" class="form-control" placeholder="nama SISWA">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Umur:</strong>
+                <input type="text" name="umur" class="form-control" placeholder="umur">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                <textarea class="form-control" style="height:150px" name="Alamat" placeholder="Content"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kota:</strong>
+                <input type="text" name="kota" class="form-control" placeholder="kota">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Kelas:</strong>
+                <input type="text" name="kelas" class="form-control" placeholder="kelas">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jurusan:</strong>
+                <input type="text" name="jurusan" class="form-control" placeholder="jurusan">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <form action="/action_page.php">
+            <p>Jenis kelamin:</p>
+              <input type="radio" id="gender" name="gender" value="laki-laki">
+              <label for="gender">laki-laki</label><br>
+              <input type="radio" id="gender" name="gender" value="perempuan">
+              <label for="gender">perempuan</label><br>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
